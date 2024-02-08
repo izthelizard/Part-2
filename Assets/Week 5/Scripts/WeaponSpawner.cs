d4f36b5f595b83b1b926c4091ee20771aa796daf
DESKTOP-7SM7S2K\Beth <larkine@sheridancollege.ca>
2024-02-07 22:27:00 -0500

    task 4b done

--- Assets/Week 5/Scripts/WeaponSpawner.cs
+++ Assets/Week 5/Scripts/WeaponSpawner.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class WeaponSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject spearPrefab;
+    public Transform spawn;
+
+    public void Spawner(GameObject spear)
     {
-        
+        Instantiate(spearPrefab, spawn);
     }
 }
